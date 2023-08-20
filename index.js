739dfa29d61057c0aadd602198ad8e5cc6231683
marcos ventura <marcosventura.dev@gmail.com>
2023-08-20 19:06:25 -0300

    Detect collision on enemy/player hit

--- index.js
+++ index.js
@@ -112,8 +112,9 @@ function spawEnemies() {
 //End spawRnemies
 
 //Início animação
+let animationId
 function animate() {
-    requestAnimationFrame(animate)
+   animationId = requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.draw()
     projectiles.forEach((projectile) => {
@@ -122,6 +123,15 @@ function animate() {
 
     enemies.forEach((enemy, index) => {
         enemy.update()
+
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+        //end game
+        if (dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId)
+            
+            }
+        //end game
+
         projectiles.forEach((projectile, projetileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
